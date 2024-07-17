0a1cf02beef35a6da816451e160b2588a92b38f2
Matheus21Oliveira <matheus21oliveira@gmail.com>
2024-07-17 12:22:26 -0300

    aula de comparação

--- EstruturaDeCompracao.lua
+++ EstruturaDeCompracao.lua
@@ -2,6 +2,8 @@
 if 
 else
 elseif    
+or
+and
 ]]
 
 soma = 6 + 5
@@ -11,3 +13,21 @@ if soma > 10 then
 else 
     print("é menor")
 end 
+
+A = 20
+B = 20
+
+if A == B or B < A then
+    print("entrou aqui")
+else 
+    print("não foi dessa vez")
+end
+
+C = 30
+D = 40
+
+if C > 40 and D > 30 then
+    print("Entrou aqui")
+else 
+    print("Nao foi dessa vez")
+end
